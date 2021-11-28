742840758b4a6c6af455c5d491277e1ec415f707
tzuchuan.huang <tzuchuan.huang@rakuten.com>
2021-11-28 12:15:57 +0800

    add git ignore and init

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(9000);
+
+module.exports = app;
